4e9119b8e3232fe9e6e9885c352463dd3c14d6dd
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-14 17:02:20 +0000

    Done.

--- test/1-Cell.test.js
+++ test/1-Cell.test.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
@@ -11,7 +12,7 @@ configure({ adapter: new Adapter() });
 
 
 describe('<Cell />', () => {
-  
+
   try {
     var Cell = require('../src/Cell').default
   } catch(e) {
@@ -21,9 +22,9 @@ describe('<Cell />', () => {
       return
     }
   }
-  
+
   let cellWhite
-  
+
   beforeEach(() => {
     cellWhite = shallow(<Cell value="#fff"/>);
   });
@@ -31,7 +32,7 @@ describe('<Cell />', () => {
   it("is a correctly defined and exported React component which renders a <div> with a className of 'cell'", () => {
     expect(cellWhite.find('.cell')).to.have.length(1)
   })
-  
+
   it("has a state key of 'color'", () => {
     expect(cellWhite.state().hasOwnProperty('color')).to.equal(true)
   })
@@ -63,6 +64,6 @@ describe('<Cell />', () => {
     cellWhite.setState({ color: '#0f0' })
     expect(cellWhite.props().style.backgroundColor).to.equal('#0f0')
   })
-  
+
 
 })
